37c35cf2967127a2aa52dc3c4755d36ac53e9bed
Vinax00 <35075793+Vinax00@users.noreply.github.com>
2018-01-13 11:09:41 -0600

    im good now

--- 2018PowerUpCode/src/org/usfirst/frc/team3103/robot/RobotMap.java
+++ 2018PowerUpCode/src/org/usfirst/frc/team3103/robot/RobotMap.java
@@ -25,8 +25,6 @@ public class RobotMap {
 	public static int blMotor = 2;
 	public static int brmotor = 3;
 
-	public static String eclipse = "sucks"; 
-
 	// If you are using multiple modules, make sure to define both the port
 	// number and the module. For example you with a rangefinder:
 	// public static int rangefinderPort = 1;
